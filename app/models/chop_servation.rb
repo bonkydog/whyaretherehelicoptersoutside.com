ff40fb207f1fb0ba805c81e731bec3f5d92c934b
Gilad Buchman <gilad.buchman@gmail.com>
2012-04-04 22:13:59 -0700

    add county location

--- app/models/chop_servation.rb
+++ app/models/chop_servation.rb
@@ -1,7 +1,14 @@
+require 'pp'
+
 class ChopServation < ActiveRecord::Base
   attr_accessor :location
   geocoded_by :location
   after_validation :geocode          # auto-fetch coordinates
-  reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode  # auto-fetch address
+  reverse_geocoded_by :latitude, :longitude do |obj,results|
+    if geo = results.first
+      pp geo.data["address_components"][4]["short_name"]
+      obj.location = geo.data["address_components"][4]["short_name"]
+    end
+  end
 end
