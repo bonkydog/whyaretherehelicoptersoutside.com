6079ce50eb3ae88074db774f120be2087e4614e4
Brian Jenkins <bonkydog@bonkydog.com>
2012-04-04 19:45:33 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hella::Application.config.secret_token = '796bc990a4d35f1aaf27b5fe8d6e5fcea8901e593473868a23759c94f0403339b99533a4fffb3269c8b1f2acf08aa23bc02506760517ad234a8cb0d43bfd5c4a'
