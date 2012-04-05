a5542981b04c2e5d9c02583b5e4dc09032435dc1
Gilad Buchman <gilad.buchman@gmail.com>
2012-04-04 21:45:37 -0700

    reasons

--- app/controllers/choppers_controller.rb
+++ app/controllers/choppers_controller.rb
@@ -1,2 +1,6 @@
 class ChoppersController < ApplicationController
+  def index
+    @chop_servations = ChopServation.all
+    p @chop_servations
+  end
 end
--- app/models/chop_servation.rb
+++ app/models/chop_servation.rb
@@ -1,2 +1,7 @@
 class ChopServation < ActiveRecord::Base
+  attr_accessor :location
+  geocoded_by :location
+  after_validation :geocode          # auto-fetch coordinates
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
 end
