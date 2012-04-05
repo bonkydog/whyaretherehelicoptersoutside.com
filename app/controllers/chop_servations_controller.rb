f0769b13162f49683512681d5e92c24bfe26b943
Thomas Fisher <thomas.fisher@keas.com>
2012-04-04 20:39:35 -0700

    persisting the chopservation

--- app/controllers/chop_servations_controller.rb
+++ app/controllers/chop_servations_controller.rb
@@ -1,6 +1,13 @@
 class ChopServationsController < ApplicationController
 
   def create
+    @chop_servation = ChopServation.new(params[:chop_servation])
+    
+    if @chop_servation.save
+      
+    else
+      
+    end
     render :nothing => true
   end
 end
